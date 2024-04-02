b6377dd8573f0857e4f6b35a84c449e4d4602c30
Subhransu Sekhar Rout <155973959+subhransu-max@users.noreply.github.com>
2024-04-02 20:22:45 +0530

    Update pattern1.java
             *   
           *   * 
         *        * 
       *             * 
    * * * * * * * * *

--- pattern1.java
+++ pattern1.java
@@ -4,26 +4,26 @@ public class pattern {
         for(int i=0;i<n;i++){
             if(i== (n-1)){
                 for (int j=0;j< 2*n-1;j++){
-                    System.out.print("*");
+                    System.out.print("* ");
                 }
             }
             else {
                 for (int space = 0; space < n - i - 1; space++) {
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
                 for (int star = 0; star < 1; star++) {
-                    System.out.print("*");
+                    System.out.print("* ");
                 }
                 for (int space2 = 0; space2 < 2 * i - 1; space2++) {
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
                 if(i != 0){
                 for (int star = 0; star < 1; star++) {
-                    System.out.print("*");
+                    System.out.print("* ");
                 }
                 }
                 else{
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
             }
             System.out.println();
